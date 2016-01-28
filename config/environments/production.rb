d6639834746b7ec54f4cf68062dc318fb265bbde
dzhaparoff <ed@netlab.pro>
2016-01-28 17:10:38 +0500

    initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,8 +52,8 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
-  config.session_store :redis_store, servers: 'redis://localhost:6379/0/cache'
+  # config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
+  # config.session_store :redis_store, servers: 'redis://localhost:6379/0/cache'
   # config.action_dispatch.rack_cache = {
   #     metastore: "redis://localhost:6379/1/metastore",
   #     entitystore: "redis://localhost:6379/1/entitystore"
