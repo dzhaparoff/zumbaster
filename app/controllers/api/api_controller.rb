85dd8e75f60a61a5d72868a7c995fc434a8bee54
dzhaparoff <ed@netlab.pro>
2016-04-10 15:37:35 +0300

    initial commit

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -15,7 +15,7 @@ class Api::ApiController < ApplicationController
     manifest = translation.manifest :desktop
     manifest = Nokogiri::XML manifest.body
     manifest.css("media").each do |media|
-      media['url'] = media['url'].gsub(/http:\/\//,'http://hd-serials.tv/stream/f4m/')
+      media['url'] = media['url'].gsub(/http:\/\//,'http://hd-serials.tv:6767/stream/f4m/')
     end
     send_data manifest.to_xml, format: 'application/xml; charset=utf-8'
   end
@@ -23,7 +23,7 @@ class Api::ApiController < ApplicationController
   private
   def create_m3u8_file translation
     manifest = translation.manifest(:mobile).body
-    manifest.gsub!(/http:\/\//, 'http://hd-serials.tv/stream/m3u8/')
+    manifest.gsub!(/http:\/\//, 'http://hd-serials.tv:6767/stream/m3u8/')
     send_data manifest, format: 'application/vnd.apple.mpegurl; charset=utf-8'
   end
 end
