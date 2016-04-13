d10ebf9e14e3255cd4cc9a435b002085e1c7b7ea
dzhaparoff <ed@netlab.pro>
2016-04-13 16:30:48 +0300

    initial commit

--- app/jobs/sync_all_photos.rb
+++ app/jobs/sync_all_photos.rb
@@ -3,7 +3,7 @@ class SyncAllPhotosJob < ProgressJob::Base
   def perform
     @trakt = Trakt.new
 
-    shows = Shows.all
+    shows = Show.all
 
     update_progress_max shows.count
     update_stage('SyncAllPhotosJob')
@@ -12,20 +12,42 @@ class SyncAllPhotosJob < ProgressJob::Base
       next unless show.ids['imdb'].to_i > 0
 
       imdb = "tt%07d" % show.ids['imdb'].to_i
-      trakt_show = @trakt.show imdb
 
-      next if trakt_show.nil?
+      trakt_seasons = @trakt.show_seasons imdb
 
-      show.poster = URI.parse(trakt_show['images']['poster']['full']) unless trakt_show['images']['poster']['full'].nil?
-      show.fanart = URI.parse(trakt_show['images']['fanart']['full']) unless trakt_show['images']['fanart']['full'].nil?
-      show.logo = URI.parse(trakt_show['images']['logo']['full']) unless trakt_show['images']['logo']['full'].nil?
-      show.clearart = URI.parse(trakt_show['images']['clearart']['full']) unless trakt_show['images']['clearart']['full'].nil?
-      show.banner = URI.parse(trakt_show['images']['banner']['full']) unless trakt_show['images']['banner']['full'].nil?
-      show.thumb = URI.parse(trakt_show['images']['thumb']['full']) unless trakt_show['images']['thumb']['full'].nil?
+      return false if trakt_seasons.nil?
 
-      show.save
+      trakt_seasons.each do |season|
+        next if season['number'] == 0
 
-      sleep 0.01
+        s = Season.where(show: show, number: season['number']).first
+
+        next if s.nil?
+
+        s.poster = URI.parse season['images']['poster']['full']
+        s.thumb = URI.parse season['images']['thumb']['full']
+        s.save
+
+        next if season['episodes'].nil?
+
+        season['episodes'].each do |episode|
+          e = Episode.where(show: show, season: s, number: episode['number']).first
+          next if e.nil?
+
+          screenshot_status = Faraday.new.get(episode['images']['screenshot']['full']).status
+          e.screenshot = URI.parse episode['images']['screenshot']['full'] if screenshot_status == 200
+
+          translation = @tvdb.episode_translation episode['ids']["tvdb"]
+
+          unless translation.nil?
+            e.title_ru = translation[:title_ru] if translation[:title_ru] != episode['title']
+            e.description_ru = translation[:description_ru]
+            e.number_abs = translation[:episode] if translation[:episode] > 0
+          end
+
+          e.save
+        end
+      end
 
       update_progress
     end
