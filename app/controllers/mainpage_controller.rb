1dcd6a8163c217a78dce8ee66d10a01267105b45
dzhaparoff <ed@netlab.pro>
2016-04-13 21:05:26 +0300

    initial commit

--- app/controllers/mainpage_controller.rb
+++ app/controllers/mainpage_controller.rb
@@ -1,36 +1,27 @@
 class MainpageController < ApplicationController
   def index
-    @new_shows = Show.order(:first_aired => :desc).take 6
-    @new_episodes = Episode.includes(:translations).order(:first_aired => :desc).where.not(translations: {id: nil}).take 12
+    @new_shows = Show.order(:first_aired => :desc).take(6)
 
-    best_kp_ratings = Rating.joins(:kp_rating).order("kp_ratings.value DESC").where("value > 8").select(:id).take 15
-    best_imdb_ratings = Rating.joins(:imdb_rating).order("imdb_ratings.value DESC").where("value > 8").select(:id).take 15
+    @new_episodes = Episode.includes(:translations)
+                        .order(:first_aired => :desc)
+                        .where("first_aired <= ?", Date.today).where.not(translations: {id: nil})
+                        .take(12)
+
+    best_kp_ratings   = Rating.joins(:kp_rating).order("kp_ratings.value DESC").where("value > 7.5").select(:id).take 15
+    best_imdb_ratings = Rating.joins(:imdb_rating).order("imdb_ratings.value DESC").where("value > 7.5").select(:id).take 15
 
     top = Show.joins(:rating)
-              .where("ratings.id in (?) and ratings.id in (?) ", best_kp_ratings, best_imdb_ratings)
+              .where("ratings.id in (?) and ratings.id in (?)", best_kp_ratings, best_imdb_ratings)
               .sort_by do |s|
                 - s.rating.imdb_rating.value * s.rating.kp_rating.value / 2
               end
+
     @top_ten = top[0...10]
 
-    random_seed = rand(Show.count)
-    @random_show = Show.offset(random_seed).take
+    rand_shows = Show.where("first_aired > ?", Date.today - 2.year)
+    random_seed = rand(rand_shows.size)
+    @random_show = rand_shows.offset(random_seed).take
 
     @popular_shows = Show.take(12)
   end
-
-  # def sitemap
-  #   path = Rails.root.join("public", "sitemaps", "sitemap.xml")
-  #   if File.exists?(path)
-  #     render xml: open(path).read
-  #   else
-  #     render text: "Sitemap not found.", status: :not_found
-  #   end
-  # end
-  #
-  # def robots
-  # end
-
-  private
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,11 +12,11 @@ module ApplicationHelper
   end
 
   def genres_list
-    Genre.order(name_ru: :asc).where.not(slug_ru: nil).all
+    Genre.joins(:shows).order(name_ru: :asc).where.not(shows: {id: nil}, slug_ru: nil).distinct
   end
 
   def random_bg_image
-    show = Show.offset(rand(Show.count)).first
+    show = Show.where("first_aired > ?", Date.today - 2.year).first
     return "" if show.nil?
     image = show.fanart
     "<img class=\"fullscreen-image\" data-canvas-image src=\"#{image.url}\" data-width=\"#{image.width}\" data-height=\"#{image.height}\"/>"\
