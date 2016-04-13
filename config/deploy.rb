4be8fdd1c9dbca96e9e12621e265841a3e13ef46
dzhaparoff <ed@netlab.pro>
2016-04-13 13:39:18 +0300

    initial commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, '/home/zumbaster'
 
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, [])
-                       .push('config/database.yml', 'config/puma.rb', 'config/config.yml', 'config/secrets.yml')
+                       .push('config/database.yml', 'config/puma.rb', 'config/config.yml', 'config/secrets.yml', 'config/application.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, [])
