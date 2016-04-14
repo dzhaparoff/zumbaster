c897749cadee658cde430019435141dde4dca604
dzhaparoff <ed@netlab.pro>
2016-04-14 11:14:42 +0300

    initial commit

--- app/jobs/sync_all_photos.rb
+++ app/jobs/sync_all_photos.rb
@@ -25,45 +25,52 @@ class SyncAllPhotosJob < ProgressJob::Base
 
         next if s.nil?
 
-        if !File.exist?(s.poster.url) && !season['images']['poster']['full'].nil?
-          poster_status = Faraday.new.get(season['images']['poster']['full']).status
-          sleep 0.1
-          s.poster = URI.parse season['images']['poster']['full'] if poster_status == 200
-          sleep 0.1
-        end
+        begin
 
-        if !File.exist?(s.thumb.url) && !season['images']['thumb']['full'].nil?
-          thumb_status = Faraday.new.get(season['images']['thumb']['full']).status
-          sleep 0.1
-          s.thumb = URI.parse season['images']['thumb']['full'] if thumb_status == 200
-          sleep 0.1
-        end
-
-        s.save
-
-        next if season['episodes'].nil?
-
-        season['episodes'].each do |episode|
-          e = Episode.where(show: show, season: s, number: episode['number']).first
-          next if e.nil? || episode['images']['screenshot']['full'].nil? || File.exist?(e.screenshot.url)
-
-          screenshot_status = Faraday.new.get(episode['images']['screenshot']['full']).status
-          sleep 0.1
-
-          e.screenshot = URI.parse episode['images']['screenshot']['full'] if screenshot_status == 200
-
-          translation = @tvdb.episode_translation episode['ids']["tvdb"]
-
-          unless translation.nil?
-            e.title_ru = translation[:title_ru] if translation[:title_ru] != '' && translation[:title_ru] != episode['title']
-            e.description_ru = translation[:description_ru] if translation[:description_ru] != ''
+          if !File.exist?(s.poster.url) && !season['images']['poster']['full'].nil?
+            poster_status = Faraday.new.get(season['images']['poster']['full']).status
+            sleep 0.1
+            s.poster = URI.parse season['images']['poster']['full'] if poster_status == 200
+            sleep 0.1
           end
 
-          e.save
+          if !File.exist?(s.thumb.url) && !season['images']['thumb']['full'].nil?
+            thumb_status = Faraday.new.get(season['images']['thumb']['full']).status
+            sleep 0.1
+            s.thumb = URI.parse season['images']['thumb']['full'] if thumb_status == 200
+            sleep 0.1
+          end
 
-          sleep 0.5
-          update_progress
+          s.save
+
+          next if season['episodes'].nil?
+
+          season['episodes'].each do |episode|
+            e = Episode.where(show: show, season: s, number: episode['number']).first
+            next if e.nil? || episode['images']['screenshot']['full'].nil? || File.exist?(e.screenshot.url)
+
+            screenshot_status = Faraday.new.get(episode['images']['screenshot']['full']).status
+            sleep 0.1
+
+            e.screenshot = URI.parse episode['images']['screenshot']['full'] if screenshot_status == 200
+
+            translation = @tvdb.episode_translation episode['ids']["tvdb"]
+
+            unless translation.nil?
+              e.title_ru = translation[:title_ru] if translation[:title_ru] != '' && translation[:title_ru] != episode['title']
+              e.description_ru = translation[:description_ru] if translation[:description_ru] != ''
+            end
+
+            e.save
+
+            sleep 0.5
+            update_progress
+          end
+
+        rescue
+          next
         end
+
       end
     end
   end
