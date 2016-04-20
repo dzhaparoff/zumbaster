3545e8187a10ad4fa9d8938fd7687ad01a7b4800
dzhaparoff <ed@netlab.pro>
2016-04-20 20:04:48 +0300

    initial commit

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,63 +1,63 @@
 class Show < ActiveRecord::Base
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  # include Elasticsearch::Model
+  # include Elasticsearch::Model::Callbacks
 
-  index_name "#{Rails.env}_shows"
-
-  def self.reindex
-    self.__elasticsearch__.create_index! force: true
-    self.__elasticsearch__.refresh_index!
-    self.import
-  end
-
-  settings index: {
-      number_of_shards: 1,
-      number_of_replicas: 0,
-      analysis: {
-          analyzer: {
-              show_analyzer: {
-                  tokenizer: "standard",
-                  filter: ["standard", "lowercase", "ru_snowball", "en_snowball"]
-              }
-          },
-          filter: {
-              ru_snowball: {
-                  type: "snowball",
-                  language: "Russian"
-              },
-              en_snowball: {
-                  type: "snowball",
-                  language: "English"
-              },
-          }
-      }
-  } do
-    mappings dynamic: 'false' do
-      indexes :id,             type: 'integer'
-      indexes :title_ru,       analyzer: 'show_analyzer', type: "text"
-      indexes :title_en,       analyzer: 'show_analyzer', type: "text"
-      indexes :description_ru, analyzer: 'show_analyzer', type: "text"
-      indexes :description_en, analyzer: 'show_analyzer', type: "text"
-      indexes :slogan_ru,      analyzer: 'show_analyzer', type: "text"
-      indexes :slogan_en,      analyzer: 'show_analyzer', type: "text"
-      indexes :network,        analyzer: 'show_analyzer', type: "text"
-      indexes :country,        analyzer: 'show_analyzer', type: "text"
-    end
-  end
-
-  def as_indexed_json(options={})
-    self.as_json(
-        only: [:id,
-               :title_ru,
-               :title_en,
-               :description_ru,
-               :description_en,
-               :slogan_ru,
-               :slogan_en,
-               :network,
-               :country]
-    )
-  end
+  # index_name "#{Rails.env}_shows"
+  #
+  # def self.reindex
+  #   self.__elasticsearch__.create_index! force: true
+  #   self.__elasticsearch__.refresh_index!
+  #   self.import
+  # end
+  #
+  # settings index: {
+  #     number_of_shards: 1,
+  #     number_of_replicas: 0,
+  #     analysis: {
+  #         analyzer: {
+  #             show_analyzer: {
+  #                 tokenizer: "standard",
+  #                 filter: ["standard", "lowercase", "ru_snowball", "en_snowball"]
+  #             }
+  #         },
+  #         filter: {
+  #             ru_snowball: {
+  #                 type: "snowball",
+  #                 language: "Russian"
+  #             },
+  #             en_snowball: {
+  #                 type: "snowball",
+  #                 language: "English"
+  #             },
+  #         }
+  #     }
+  # } do
+  #   mappings dynamic: 'false' do
+  #     indexes :id,             type: 'integer'
+  #     indexes :title_ru,       analyzer: 'show_analyzer', type: "text"
+  #     indexes :title_en,       analyzer: 'show_analyzer', type: "text"
+  #     indexes :description_ru, analyzer: 'show_analyzer', type: "text"
+  #     indexes :description_en, analyzer: 'show_analyzer', type: "text"
+  #     indexes :slogan_ru,      analyzer: 'show_analyzer', type: "text"
+  #     indexes :slogan_en,      analyzer: 'show_analyzer', type: "text"
+  #     indexes :network,        analyzer: 'show_analyzer', type: "text"
+  #     indexes :country,        analyzer: 'show_analyzer', type: "text"
+  #   end
+  # end
+  #
+  # def as_indexed_json(options={})
+  #   self.as_json(
+  #       only: [:id,
+  #              :title_ru,
+  #              :title_en,
+  #              :description_ru,
+  #              :description_en,
+  #              :slogan_ru,
+  #              :slogan_en,
+  #              :network,
+  #              :country]
+  #   )
+  # end
 
 
   has_and_belongs_to_many :genres, :join_table => :genres_shows
