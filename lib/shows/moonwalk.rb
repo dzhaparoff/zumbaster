3e02314040b7a93276b0da86eb9557d4fc174aed
Enver Dzhaparoff <ed@netlab.pro>
2017-03-06 12:28:50 +0300

    initial commit

--- lib/shows/moonwalk.rb
+++ lib/shows/moonwalk.rb
@@ -62,7 +62,7 @@ class Moonwalk
 
 
   def self.get_iframe_page(iframe,s,e)
-    f = Faraday.new(url: 'http://s1.staticnlcdn.com') do |builder|
+    f = Faraday.new(url: 'http://previewer.cc') do |builder|
       builder.use     :cookie_jar
       builder.adapter :net_http
       builder.request :url_encoded
@@ -70,8 +70,8 @@ class Moonwalk
       builder.headers['Accept-Language'] = "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4,bg;q=0.2,de;q=0.2,es;q=0.2,fr;q=0.2,it;q=0.2,mk;q=0.2,tr;q=0.2"
       builder.headers['Cache-Control'] = "max-age=0"
       builder.headers['Connection']    = "keep-alive"
-      builder.headers['Host']          = "moonwalk.cc"
-      builder.headers['Referer']       = "http://s1.staticnlcdn.com/"
+      builder.headers['Host']          = "previewer.cc"
+      builder.headers['Referer']       = "http://previewer.cc/"
       builder.headers['Upgrade-Insecure-Requests'] = "1"
       builder.headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36"
     end
@@ -88,22 +88,23 @@ class Moonwalk
     playlist_request = faraday.post do |b|
       b.url '/sessions/new_session'
       b.headers['Connection'] = 'keep-alive'
-      b.headers['Host'] = 'moon.hdkinoteatr.com'
-      b.headers['Origin'] = 'http://s1.staticnlcdn.com'
-      b.headers['Referer'] = referer.sub!("moonwalk.cc", "s1.staticnlcdn.com")
-      b.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'
+      b.headers['Host'] = 'previewer.cc'
+      b.headers['Origin'] = 'http://previewer.cc'
+      b.headers['Referer'] = referer.sub!("moonwalk.cc", "previewer.cc")
+      b.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
       b.headers['Accept'] = '*/*'
       b.headers['Accept-Encoding'] = 'gzip, deflate'
       b.headers['Accept-Language'] = 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4,bg;q=0.2,de;q=0.2,es;q=0.2,fr;q=0.2,it;q=0.2,mk;q=0.2,tr;q=0.2'
+
       b.headers['X-CSRF-Token'] = csrf_token
       b.headers['X-Requested-With'] = 'XMLHttpRequest'
-      b.headers['X-Condition-Safe'] = 'Normal'
+      b.headers['X-Bool-Ray'] = 'XRAY'
 
       b.body = URI.encode_www_form({
                                       #  mw_domain_id: 26873,
                                        video_token: video_token,
                                        content_type: 'serial',
-                                       mw_key: '1152сb1dd4c4d544',
+                                       mw_key: 'f5a9fc6888e7bc5cb41ffd4aa558cc51',
                                        mw_pid: 918,
                                        p_domain_id: 26873,
                                        ad_attr: 0,
@@ -111,7 +112,7 @@ class Moonwalk
                                        debug: false
                                    })
     end
-    
+
     JSON.parse playlist_request.body
   end
 
