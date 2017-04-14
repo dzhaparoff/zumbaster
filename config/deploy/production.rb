76c5a6303760f0bdeadf9318f76ebd8d56b58fe4
Enver Dzhaparoff <ed@netlab.pro>
2017-04-14 15:33:28 +0300

    init commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,8 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 # server '195.123.209.149', user: 'zumbaster', roles: %w{web app db}
-server '195.123.214.11', user: 'zumbaster', roles: %w{web app db}
+# server '195.123.214.11', user: 'zumbaster', roles: %w{web app db}
+server '195.123.209.149', user: 'zumbaster', roles: %w{web app db}
 
 
 # role-based syntax
