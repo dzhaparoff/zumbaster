e9d01ff263e3064267b8b09e22d59be27baea345
Enver Dzhaparoff <ed@netlab.pro>
2017-04-18 17:25:01 +0300

    init commit

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -14,16 +14,16 @@ class Api::ApiController < ApplicationController
   def create_f4m_file translation
     manifest = translation.manifest :desktop
     manifest = Nokogiri::XML manifest.body
-    # manifest.css("media").each do |media|
-    #   media['url'] = media['url'].gsub(/http:\/\//,'/stream/')
-    # end if Rails.env.production?
+    manifest.css("media").each do |media|
+      media['url'] = media['url'].gsub(/http:\/\//,'/stream/')
+    end if Rails.env.production?
     send_data manifest.to_xml, format: 'application/xml; charset=utf-8'
   end
 
   private
   def create_m3u8_file translation
     manifest = translation.manifest(:mobile).body
-    # manifest.gsub!(/http:\/\//, '/stream/') if Rails.env.production?
+    manifest.gsub!(/http:\/\//, '/stream/') if Rails.env.production?
     send_data manifest, format: 'application/vnd.apple.mpegurl; charset=utf-8'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031102802) do
+ActiveRecord::Schema.define(version: 20170417162927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,17 @@ ActiveRecord::Schema.define(version: 20161031102802) do
     t.integer  "progress_current", default: 0
     t.integer  "progress_max",     default: 0
     t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+  end
+
+  create_table "donations", force: :cascade do |t|
+    t.string   "ex_id"
+    t.string   "name"
+    t.string   "amount"
+    t.string   "comment"
+    t.datetime "donation_date"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["ex_id"], name: "index_donations_on_ex_id", using: :btree
   end
 
   create_table "episodes", force: :cascade do |t|
