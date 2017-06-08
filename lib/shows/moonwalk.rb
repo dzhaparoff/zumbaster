ea4a97b0ffc897f734ae1640100b75ff52db838f
Enver Dzhaparoff <ed@netlab.pro>
2017-06-08 21:45:22 +0300

    init commit

--- lib/shows/moonwalk.rb
+++ lib/shows/moonwalk.rb
@@ -111,8 +111,8 @@ class Moonwalk
                                        video_token: video_token,
                                        content_type: 'serial',
                                        mw_key: '1ffd4aa558cc51f5a9fc6888e7bc5cb4',
-                                       mw_pid: 2876,
-                                       p_domain_id: 435094,
+                                       mw_pid: 918,
+                                       p_domain_id: 455971,
                                        ad_attr: 0,
                                        "#{argv_name}": argv_value
                                    })
